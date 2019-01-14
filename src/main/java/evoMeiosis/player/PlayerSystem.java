d9ab6abf116b53685d8710e5baa1c7ffb3c44915
TommyKriz <lizardman420davidicke.com@gmail.com>
2019-01-14 02:35:00 +0100

    fixed player leaving and reentering error

--- src/main/java/evoMeiosis/player/PlayerSystem.java
+++ src/main/java/evoMeiosis/player/PlayerSystem.java
@@ -56,6 +56,8 @@ public class PlayerSystem {
 			}
 		}
 
+		activeIds = getActiveIds();
+		
 		ArrayList<Integer> removePlayers = new ArrayList<>();
 
 		if (activePlayers.size() > tuioPlayers.size()) {
